07f2ab3957900ec0f0281eb2795638b49a39ea56
cyan <cyan@ripple.com>
2024-04-02 15:43:57 +0100

    try to fix memory

--- tools/db_checker/internal/utils/utils.go
+++ tools/db_checker/internal/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 // #include "utils.h"
+// #include <stdlib.h>
 import "C"
 import (
 	"encoding/hex"
@@ -22,6 +23,8 @@ func GetStatesHashFromLedgerHeader(blob string, size uint32) string {
 
 func GetLedgerHashFromLedgerHeader(blob string, size uint32) string {
 	cbytes := make([]byte, 32)
-	C.GetLedgerHashFromLedgerHeader(C.CString(blob), C.int(size), (*C.char)(unsafe.Pointer(&cbytes[0])))
+	cblob := C.CString(blob)
+	C.GetLedgerHashFromLedgerHeader(cblob, C.int(size), (*C.char)(unsafe.Pointer(&cbytes[0])))
+	C.free(unsafe.Pointer(cblob))
 	return strings.ToUpper(hex.EncodeToString(cbytes))
 }
