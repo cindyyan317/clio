4db06f8f1021fbc19c5cf8c28361e2e5bd38a3e3
cyan <cyan@ripple.com>
2024-04-24 14:54:38 +0100

    add sh helper

---
+++ tools/db_checker/run_fix_nft_uri.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This script is used to fix the nf_token_uris in the database. 
+# parameters: db_checker location and the start seq
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <db_checker_location> <start_seq>"
+    exit 1
+fi
+
+db_checker=$1
+start_seq=$2
+
+
+db_cluster=""
+step=500000
+password=""
+username=""
+keyspace=""
+db_step=8
+
+while [ $start_seq -gt 0 ]; do
+    end_seq=$(($start_seq - $step))
+    if [ $end_seq -lt 0 ]; then
+        end_seq=0
+    fi
+    echo "Fixing ledgerhash from $start_seq to $end_seq"
+    $db_checker --tx --NFTUriFix --username $username --password $password --keyspace $keyspace --fromLedgerIdx $end_seq  --toLedgerIdx $start_seq --step $db_step $db_cluster > ~/fix_ledgerhash_${end_seq}_${start_seq}.log 2>&1 
+    if [ $? -ne 0 ]; then
+        echo "Failed to fix ledgerhash from $start_seq to $end_seq"
+        exit 1
+    fi
+    start_seq=$end_seq
+done
+
+
